18953f24eb8dbb796bfc842b8cf91e2a0f6344f5
Sean P <perez.seanneptali.3@gmail.com>
2026-03-10 13:01:29 +0800

    lab-act1

--- perez-client/src/App.jsx
+++ perez-client/src/App.jsx
@@ -4,31 +4,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my React App</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Name: Sean Perez <br />
+          Email: perezsp1@students.national-u.edu.ph <br />
+          Course: BSIT <br />
+          Other Personal Info: <a href='https://github.com/PerezSN/perez-webprog' target='_blank'>GitHub</a> <br />
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </header>
+    </div>
   )
 }
 
